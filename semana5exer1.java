272f40e59b25f1b247b9fb3ab8defd9bd71e0f52
Raphael Leal <raphaelleal990@gmail.com>
2021-09-03 01:21:49 +0000

    Atualiazado

---
+++ semana5exer1.java
@@ -0,0 +1,11 @@
+public class semana5exer1{
+    public static void main(String [] args){
+        long fat=1;
+        for(int n=20;n>=1;n--) {
+             fat=fat*n;
+        }
+        System.out.println(fat);
+
+    }
+
+}
